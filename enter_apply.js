c16f891ead25f9df6cd85a57781972e2dca6967d
chenhaoyuan <chenhaoyuan@live.cn>
2022-10-18 16:28:08 +0800

    add content

---
+++ enter_apply.js
@@ -0,0 +1,42 @@
+var applyForm = document.getElementById('userName');
+var generateButton = document.getElementById('generate-qrcode');
+var qrcode = new QRCode(document.getElementById("display-qrcode"), {
+    text: '',
+    //width: 128,
+    //height: 128,
+    colorDark : "#000000",
+    colorLight : "#ffffff",
+    //correctLevel : QRCode.CorrectLevel.H
+});
+qrcode.clear();
+
+
+if (generateButton) {
+    generateButton.addEventListener('click', function () {
+        qrcode.clear();
+        var qrcodeValueList = document.getElementsByClassName('qrcode-value')
+        
+        var result = "";
+        for(var qrcodeValue of qrcodeValueList) {
+            if(qrcodeValue.value.length == 0){
+                console.log("value is empty");
+                return;
+            }
+
+            if(result.length == 0){
+                result = qrcodeValue.value;
+            }else{
+                result = result +"$$"+ qrcodeValue.value;
+            }
+        }
+        console.log(result)
+        qrcode.makeCode(result); // make another code.
+
+  })
+}
+
+document.getElementById('apply-form').onsubmit= function(e){
+    e.preventDefault();
+}
+
+
