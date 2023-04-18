ee4291592c3dbe667ff0a8c9482598a251af0233
chenhaoyuan <chenhaoyuan@live.cn>
2023-04-18 10:47:59 +0800

    delete old page

--- scripts/enter_apply.js
+++ scripts/enter_apply.js
@@ -1,43 +1,45 @@
 var generateButton = document.getElementById('generate-qrcode');
-var qrcode = new QRCode(document.getElementById("display-qrcode"), {
-    text: '测试数据测试数据测试数据测试数据测试数据测试数据测试数据测试数据测试数据测试数据测试数据',
-    //width: 128,
-    //height: 128,
-    colorDark : "#000000",
-    colorLight : "#ffffff",
-    //correctLevel : QRCode.CorrectLevel.H
-});
-qrcode.clear();
-
 
 if (generateButton) {
     generateButton.addEventListener('click', function () {
-        qrcode.clear();
-
         var qrcodeValueList = document.getElementsByClassName('qrcode-value')
         var result = {};
-        for(var qrcodeValue of qrcodeValueList) {
-            if(qrcodeValue.value.length == 0){
+        for (var qrcodeValue of qrcodeValueList) {
+            if (qrcodeValue.value.length == 0) {
                 console.log("value is empty");
                 alert("请填写完整。");
                 return;
             }
-
             result[qrcodeValue.id] = qrcodeValue.value;
+            document.cookie = qrcodeValue.id + "=" + qrcodeValue.value;
         }
-        console.log(result)
+        result["date"] = new Date();
+        result["validTime"] = "m5";
+        result["codeId"] = genNonDuplicateID();
+        console.log(result);
         var encryptResult = Encrypt(JSON.stringify(result))
         console.log(encryptResult)
-        qrcode.makeCode(encryptResult); // make another code.
 
-        var image = document.getElementById("display-qrcode").getElementsByTagName("img")[0].src;
 
-        var a = document.createElement("a"); //Create <a>
-        a.href = image; //Image Base64 Goes here
-        a.download = "Image.png"; //File name Here
-        a.click(); //Downloaded file
+        var qr = new QRious({
+            element: document.getElementById('qrcode_canvas'),
+            size: 300,
+            level: "Q",
+            value: encryptResult
+        });
+        imageUrl = qr.toDataURL();
 
-  })
+        var imageElement = document.getElementById('image_display');
+        imageElement.src = imageUrl;
+
+        var noteP = document.getElementById('note-p');
+        noteP.removeAttribute("hidden");
+    })
+    //从cookie中设置默认值
+    var qrcodeValueList = document.getElementsByClassName('qrcode-value')
+    for (var qrcodeValue of qrcodeValueList) {
+        qrcodeValue.value = getCookie(qrcodeValue.id);
+    }
 }
 
 var key = CryptoJS.enc.Utf8.parse("1234123412ABCDEF");  //十六位十六进制数作为密钥
@@ -49,3 +51,43 @@ function Encrypt(word) {
     return encrypted.toString();
 }
 
+
+//汉字生成二维码前要转换，AES编码后则不需要
+function utf16to8(str) {
+    var out, i, len, c;
+    out = "";
+    len = str.length;
+    for (i = 0; i < len; i++) {
+        c = str.charCodeAt(i);
+        if ((c >= 0x0001) && (c <= 0x007F)) {
+            out += str.charAt(i);
+        } else if (c > 0x07FF) {
+            out += String.fromCharCode(0xE0 | ((c >> 12) & 0x0F));
+            out += String.fromCharCode(0x80 | ((c >> 6) & 0x3F));
+            out += String.fromCharCode(0x80 | ((c >> 0) & 0x3F));
+        } else {
+            out += String.fromCharCode(0xC0 | ((c >> 6) & 0x1F));
+            out += String.fromCharCode(0x80 | ((c >> 0) & 0x3F));
+        }
+    }
+    return out;
+}
+
+function getCookie(cname)
+{
+  var name = cname + "=";
+  var ca = document.cookie.split(';');
+  for(var i=0; i<ca.length; i++) 
+  {
+    var c = ca[i].trim();
+    if (c.indexOf(name)==0) return c.substring(name.length,c.length);
+  }
+  return "";
+}
+
+/**
+ * 生成一个用不重复的ID
+ */
+ function genNonDuplicateID(randomLength){
+    return Number(Math.random().toString().substr(2,4) + Date.now()).toString(16)
+  }
